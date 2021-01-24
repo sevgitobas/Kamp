1809f8ed532b62fa07c1000959ea24285bbc79b1
SEVGİ <SEVG@DESKTOP-FLEASTC>
2021-01-24 17:08:42 +0300

    23 ocak ders sonu

---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Coorporate ya da legal entity
+    //inheritance - burada tüzel müşterinin aynı zamanda müşteri olduğunu yazdık.
+    class TuzelMusteri:Musteri
+    {
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService(); 
+            //eğer burayı yazmak istemiyorsan aşağıda yazarken new SmsLoggerService() yazman yeterli. 
+            //Burada önemli olan hafızada bir yer açmak.
+
+            List<ILoggerService> loggers = new List<ILoggerService>() { databaseLoggerService, smsLoggerService };
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
